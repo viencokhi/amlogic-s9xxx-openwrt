83f7d4d9c415bfeeaa7d5f9865dead04490a02c1
viencokhi <63581077+viencokhi@users.noreply.github.com>
2022-02-05 01:50:37 +0700

    add custom feeds

--- router-config/openwrt-21.02/diy-part2.sh
+++ router-config/openwrt-21.02/diy-part2.sh
@@ -45,7 +45,8 @@ svn co https://github.com/hubutui/p7zip-lede/trunk package/lean/p7zip
 # svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
 # Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
 # sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
-
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo-packages
+git clone https://github.com/kenzok8/small.git package/kenzo-small
 # Apply patch
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
